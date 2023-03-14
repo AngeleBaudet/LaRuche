497fa5598ed7a75eed676a17a672a0843ca8e15f
ChloeD <chloe.desflaches@gmail.com>
2023-03-14 10:50:13 +0100

    post method

--- Projet-Ruche/src/main/java/fr/ruche/api/UserApiController.java
+++ Projet-Ruche/src/main/java/fr/ruche/api/UserApiController.java
@@ -2,18 +2,25 @@ package fr.ruche.api;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
 import fr.ruche.dao.IDAOUser;
+import fr.ruche.exception.UserBadRequestException;
 import fr.ruche.exception.UserNotFoundException;
-import fr.ruche.model.User;
+import fr.ruche.model.*;
+import fr.ruche.request.UserRequest;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
@@ -34,6 +41,27 @@ public class UserApiController {
 	public List<User> findAll() {
 		return this.daoUser.findAll();
 	}
+	
+	@PostMapping
+	public User add(@RequestBody @Valid UserRequest userRequest, BindingResult results) {
+		if (results.hasErrors()) {
+			throw new UserBadRequestException();
+		}
+		
+		User user; 
+		if (userRequest.getType().equalsIgnoreCase("gestionnaire")) {
+			user = new Gestionnaire(); 	
+		}
+		else if (userRequest.getType().equalsIgnoreCase("recolteur")) {
+			user = new Recolteur(); 	
+		}
+		else {
+			user = new Client(); 
+		}
+		
+		BeanUtils.copyProperties(userRequest, user); 
+		return this.daoUser.save(user); }
+
 	
 	
 }
--- Projet-Ruche/src/main/java/fr/ruche/request/UserRequest.java
+++ Projet-Ruche/src/main/java/fr/ruche/request/UserRequest.java
@@ -1,5 +1,31 @@
 package fr.ruche.request;
 
 public class UserRequest {
-
+	
+	private String password; 
+	private String login ; 
+	private String type ; //gestionnaire, client, recolteur
+	
+	
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
 }
