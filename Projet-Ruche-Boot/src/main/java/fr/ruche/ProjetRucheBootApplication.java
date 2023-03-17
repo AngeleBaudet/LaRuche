898784e3327cc1adec186f004138cb68a1ce5891
julienkot <julienkot.c@gmail.com>
2023-03-17 15:03:48 +0100

    Spring boot la ruche folder

---
+++ Projet-Ruche-Boot/src/main/java/fr/ruche/ProjetRucheBootApplication.java
@@ -0,0 +1,13 @@
+package fr.ruche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetRucheBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetRucheBootApplication.class, args);
+	}
+
+}
