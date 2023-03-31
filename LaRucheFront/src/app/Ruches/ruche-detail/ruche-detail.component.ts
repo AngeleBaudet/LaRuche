eef5d554411a9c7b0a4a9d82cd69a79aed28174c
ChloeD <chloe.desflaches@gmail.com>
2023-03-31 23:53:59 +0200

    add ruche form

--- LaRucheFront/src/app/Ruches/ruche-detail/ruche-detail.component.ts
+++ LaRucheFront/src/app/Ruches/ruche-detail/ruche-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Ruche } from 'src/app/model';
+import { RucheHttpService } from '../ruche-http.service';
 
 @Component({
   selector: 'ruche-detail',
   templateUrl: './ruche-detail.component.html',
   styleUrls: ['./ruche-detail.component.scss']
 })
-export class RucheDetailComponent {
+export class RucheDetailComponent implements OnInit {
+  
+  cetteRuche : Ruche;
+  rucheForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private rucheService: RucheHttpService) {}
+
+  ngOnInit(): void {
+    this.rucheForm = this.formBuilder.group({
+      email: this.formBuilder.control('', [Validators.required, Validators.email]),
+      password: this.formBuilder.control('', [Validators.required, Validators.minLength(8)]),
+      passwordConfirm: this.formBuilder.control('', Validators.required)
+    });
+  }
+
+  submit(){}
+
+  list(){
+    return this.rucheService.findAll();
+  }
 
 }
