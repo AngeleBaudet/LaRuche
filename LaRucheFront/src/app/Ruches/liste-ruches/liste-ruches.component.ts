e2d1f0f55c41274f3510b4403744c4fefe80bde3
Angele <baudet.angele@gmail.com>
2023-04-01 16:42:39 +0200

    Début ListeRuches

--- LaRucheFront/src/app/Ruches/liste-ruches/liste-ruches.component.ts
+++ LaRucheFront/src/app/Ruches/liste-ruches/liste-ruches.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-liste-ruches',
+  selector: 'liste-ruches',
   templateUrl: './liste-ruches.component.html',
   styleUrls: ['./liste-ruches.component.scss']
 })
