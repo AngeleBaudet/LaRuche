6c041bbd0f2b05ac6cfd7f7b30a1088f296ce16e
Angele <baudet.angele@gmail.com>
2023-04-03 15:33:56 +0200

    Component production / user

---
+++ LaRucheFront/src/app/Production/liste-production/liste-production.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-production',
+  templateUrl: './liste-production.component.html',
+  styleUrls: ['./liste-production.component.scss']
+})
+export class ListeProductionComponent {
+
+}
