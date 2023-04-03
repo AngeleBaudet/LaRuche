76cc93e22de4c4f84341e242a6ebef184fe7bc69
julienkot <julienkot.c@gmail.com>
2023-04-03 15:23:40 +0200

    still have issues vulne, nourrissage

---
+++ LaRucheFront/src/app/accueil/accueil-http.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, Query } from '@angular/core';
+import { Ruche } from '../model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { GlobalService } from '../global.service';
+import { RucheHttpService } from '../Ruches/ruche-http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccueilHttpService {
+
+  private rucheApiPath: string;
+  listRucheVulnerabilite: Array<Ruche> = new Array<Ruche>;
+
+  listRucheNourissage: Array<Ruche> = new Array<Ruche>;
+
+  constructor(private http: HttpClient, 
+    private rucheService: RucheHttpService,private globalService: GlobalService)
+    { 
+    this.rucheApiPath = globalService.apiPath + "ruche";
+}
+
+findRucheByRecolteur(id: number): Observable<Array<Ruche>> {
+  return this.http.get<Array<Ruche>>(this.rucheApiPath + "/" + id);
+}
+
+  
+}
--- LaRucheFront/src/app/accueil/accueil.component.ts
+++ LaRucheFront/src/app/accueil/accueil.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { RucheHttpService } from '../Ruches/ruche-http.service';
+import { AccueilHttpService } from './accueil-http.service';
+import { Gestionnaire, Ruche } from '../model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'accueil',
@@ -7,4 +13,35 @@ import { Component } from '@angular/core';
 })
 export class AccueilComponent {
 
+
+  private rucheApiPath: string;
+  listRucheVulnerabilite: Array<Ruche> = new Array<Ruche>;
+
+/*   listRucheNourissage: Array<Ruche> = new Array<Ruche>; */
+
+  constructor(private AccueilService: AccueilHttpService){ }
+
+findRucheNourissage(id:number): Array<Ruche>{
+  listRucheNourissage : Array<Ruche> = this.AccueilService.findRucheByRecolteur(id);
+
+  return listRucheNourissage;
+
 }
+
+findRucheVulnerabilite(id: number): Array<Ruche> {
+  let listRucheVulnerabilite: Array<Ruche> = [];
+
+  this.AccueilService.findRucheByRecolteur(id).subscribe(
+    (ruches: Ruche[]) => {
+      listRucheVulnerabilite = ruches.filter(ruche => ruche.vulnerabilite!= null);
+    },
+    (error: any) => {
+      console.log("Error fetching Ruches: ", error);
+    }
+  );
+
+  return listRucheVulnerabilite;
+}
+}
+
+
