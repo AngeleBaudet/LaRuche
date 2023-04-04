17ec1badc05049101cdaeb0a75cc0c73ebaea629
Lina Allel <lina.allel@live.fr>
2023-04-04 14:56:40 +0200

    MAJ FindRucheByRecolteur

--- LaRucheFront/src/app/Ruches/ruche-http.service.ts
+++ LaRucheFront/src/app/Ruches/ruche-http.service.ts
@@ -3,34 +3,41 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Ruche, RucheRequest } from '../model';
 import { GlobalService } from '../global.service';
+import { ConnexionHttpService } from '../connexion/connexion-http.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RucheHttpService {
-  
   private ruches: Array<Ruche> = new Array<Ruche>();
   private rucheApiPath: string;
+  private rucheByRecolteur: Array<Ruche> = new Array<Ruche>();
 
-  constructor(private http: HttpClient, private globalService: GlobalService) {
-    this.rucheApiPath = globalService.apiPath + "ruche";
+  constructor(
+    private http: HttpClient,
+    private globalService: GlobalService,
+    private connexionService: ConnexionHttpService
+  ) {
+    this.rucheApiPath = globalService.apiPath + 'ruche';
     this.load();
   }
 
   findAll(): Array<Ruche> {
-    return this.ruches;
+    if (this.connexionService.connectedUser.type === 'recolteur') {
+      return this.rucheByRecolteur;
+    } else return this.ruches;
   }
 
   findById(id: number): Observable<Ruche> {
-    return this.http.get<Ruche>(this.rucheApiPath + "/" + id);
+    return this.http.get<Ruche>(this.rucheApiPath + '/' + id);
   }
 
   create(ruche: RucheRequest): void {
-    this.http.post<RucheRequest>(this.rucheApiPath, ruche).subscribe(resp => {
+    this.http.post<RucheRequest>(this.rucheApiPath, ruche).subscribe((resp) => {
       this.load();
     });
   }
- 
+
   /*exemple requete correcte : {
     "cadre": 5,
     "limite": false,
@@ -39,20 +46,33 @@ export class RucheHttpService {
   }*/
 
   update(ruche: RucheRequest): void {
-    this.http.put<RucheRequest>(this.rucheApiPath + "/" +  ruche.id, ruche).subscribe(resp => {
-      this.load();
-    });
+    this.http
+      .put<RucheRequest>(this.rucheApiPath + '/' + ruche.id, ruche)
+      .subscribe((resp) => {
+        this.load();
+      });
   }
 
   remove(id: number): void {
-    this.http.delete<boolean>(this.rucheApiPath + "/" + id).subscribe(resp => {
-      this.load();
-    });
+    this.http
+      .delete<boolean>(this.rucheApiPath + '/' + id)
+      .subscribe((resp) => {
+        this.load();
+      });
   }
 
   private load(): void {
-    this.http.get<Array<Ruche>>(this.rucheApiPath).subscribe(resp => {
-        this.ruches = resp;
-    })
+    this.http.get<Array<Ruche>>(this.rucheApiPath).subscribe((resp) => {
+      this.ruches = resp;
+    });
+    if (this.connexionService.connectedUser.type === 'recolteur') {
+      this.http
+        .get<Array<Ruche>>(
+          this.rucheApiPath +
+            'by-recolteur-id/' +
+            this.connexionService.connectedUser.id
+        )
+        .subscribe((resp) => (this.rucheByRecolteur = resp));
+    }
   }
 }
