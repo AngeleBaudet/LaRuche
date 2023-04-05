65928f12ca5415aa7defb2c5fa2c19311e552036
Lina Allel <lina.allel@live.fr>
2023-04-05 11:49:32 +0200

    Update correction front

--- Projet-Ruche-Boot/src/main/java/fr/ruche/dao/IDAORuche.java
+++ Projet-Ruche-Boot/src/main/java/fr/ruche/dao/IDAORuche.java
@@ -10,7 +10,7 @@ import fr.ruche.model.Ruche;
 
 public interface IDAORuche extends JpaRepository<Ruche, Integer>{
 
-	@Query("SELECT r from Ruche r join fetch r.productions p where p.produit in ('Miel','Pollen') Group by r having Sum(p.stock)<=12")
+	@Query("SELECT r from Ruche r join fetch r.productions p where p.produit in ('Miel','Pollen') Group by r having Sum(p.stock)>=12")
 	public List<Ruche> findRucheByNourissage();
 	
 	@Query("SELECT r FROM Ruche r JOIN r.recolteur u WHERE u.id = ?1")
